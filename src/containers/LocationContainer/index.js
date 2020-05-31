46946bb446cb32929eaf2510babdab1319433c06
Athira <athirafrugalscientific>
2020-05-31 10:37:07 +0530

    Redux  configaration completed

--- src/containers/LocationContainer/index.js
+++ src/containers/LocationContainer/index.js
@@ -2,13 +2,20 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 
 import Location from '../../screens/Location/index'
-
+import { location } from './action'
 class LocationContainer extends React.Component<Props, State>{
     render() {
-        return <Location />
+        return <Location fetchlocation={this.props.fetchlocation} />
     }
 }
-
+function bindAction(dispatch) {
+    return {
+        fetchlocation: () => {
+            console.log("index work")
+            dispatch(location())
+        }
+    }
+}
 const mapStateToProps = state => ({})
 
-export default connect(mapStateToProps, null)(LocationContainer)
+export default connect(mapStateToProps, bindAction)(LocationContainer)
